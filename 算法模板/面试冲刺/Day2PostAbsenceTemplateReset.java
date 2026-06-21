c6dd6e02bfa0828b82b186fcb68cf761192dc0cd
ynot777 <2034037471@qq.com>
2026-06-21 15:51:58 +0800

    study(day2): add post-absence template reset

---
+++ 算法模板/面试冲刺/Day2PostAbsenceTemplateReset.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day2PostAbsenceTemplateReset {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        for (int value : freq) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groups = new HashMap<>();
+        for (String str : strs) {
+            int[] freq = new int[26];
+            for (char c : str.toCharArray()) {
+                freq[c - 'a']++;
+            }
+            String key = Arrays.toString(freq);
+            groups.computeIfAbsent(key, ignored -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(groups.values());
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(0, 1);
+        int prefix = 0;
+        int answer = 0;
+        for (int num : nums) {
+            prefix += num;
+            answer += count.getOrDefault(prefix - goal, 0);
+            count.put(prefix, count.getOrDefault(prefix, 0) + 1);
+        }
+        return answer;
+    }
+}
